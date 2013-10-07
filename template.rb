d333fe4cba49f9cd96d462508b2009ba11297337
Phill Baker <phillbaker@retrodict.com>
2013-10-06 23:48:26 -0400

    Update template.rb

--- template.rb
+++ template.rb
@@ -2,25 +2,63 @@
 ## Gems
 #
 
+group :deployment do
+  gem 'capistrano'
+end
+
+# Auth (authentication + authorization)
+group :auth do
+  gem 'devise'
+  gem 'cancan'
+end
+
 gem_group :maintenance do
   gem 'turnout'
 end
 
-gem_group :security do
-  gem 'devise' # Authentication
-  gem 'cancan' # Authorization via roles
+gem_group :api do
+  gem 'fog' # Access to multiple cloud services
 end
 
-gem_group :rails_helpers do
-  gem 'obfuscate_id' # Don't expose DB id's to users
-  # gem 'friendly_id' # URL slugs
+gem_group :async_processing do
+  # Some in-process ones to start
+  # gem 'rufus-scheduler'
+  # gem 'sucker_punch'
+end
 
+gem_group :ab_testing do
+  # gem 'vanity'
+end
+
+gem_group :models do
+  gem 'friendly_id' # URL slugs
+  gem 'obfuscate_id' # Don't expose object ids to users
+  gem 'better_serialization' # Replace Yaml serialization
+end
+
+gem_group :controllers do
+  gem 'cocoon' # Nested objects forms
+end
+
+gem_group :views do
+  gem 'meta-tags', require: 'meta_tags'
   gem 'simple_form'
-  # gem 'cocoon' # Nested models
+  gem 'will_paginate'
   gem 'breadcrumbs_on_rails'
   gem 'inherited_resources' # Cleaner controllers
-  gem 'rails_autolink' # View helper for sanitized html_safe strings with URLs + e-mails
-  gem 'better_serialization' # Replace Yaml serialization
+end
+
+gem_group :input do
+  gem 'rails_autolink' # Sanitized html_safe strings with URLs + e-mails
+end
+
+gem_group :generators do
+  # gem 'rabl' # JSON render-er
+  gem 'sitemap_generator', require: false
+end
+
+gem_group :monitoring do
+  # gem 'nunes' # ActiveSupport::Notifications => statsd
 end
 
 gem_group :uploads do
@@ -33,58 +71,74 @@ end
 gem_group :assets do
   gem 'csso-rails' # CSS Optimizer
   gem 'coffee-rails'
-  gem 'less-rails' # Do before sass-rails so that scaffolding is done with less
+  gem 'less-rails'
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
   gem 'therubyracer', platforms: :ruby
   gem 'uglifier', '>= 1.0.3'
   gem 'sprockets'
   gem 'sprockets-image_compressor'
   gem 'favicon_maker' # Hi and low res fav-icons for asset-pipeline
+  gem 'asset_sync' # Move compiled assets off of app servers
 end
 
 gem_group :client_side do 
   gem 'jquery-rails'
   gem 'jquery-ui-rails'
-  gem 'rails-backbone'
-  gem 'jquery-fileupload-rails'
+  # gem 'jquery-fileupload-rails'
   
   gem 'less-rails-bootstrap'
   # vs. twitter-bootstrap-rails ?
 end
 
-gem_group :development do
-  # Logging
+gem_group :development_logging do
   gem 'quiet_assets'
   gem 'lograge'
-  
-  # Server
+end
+
+gem_group :development_server do
   gem 'thin'
+end
+
+gem_group :development_mail do
+  gem 'mailcatcher' # Fake SMTP/email client
+end
   
-  # Fake SMTP/email client
-  gem 'mailcatcher'
-  
-  gem 'bullet' # Avoid N+1 queries
+gem_group :development_database do
+  gem 'bullet' # Catch N+1 queries
 end
 
 gem_group :test do
   gem 'shoulda-matchers'
   gem 'email_spec'
   gem 'timecop'
-  gem 'vcr'
+  gem 'vcr' # For requests that are hard to mock.
+  gem 'fakeweb'
+end
 
-  # Cucumber feature tests
+gem_group :spec do
+  gem 'rspec-rails'
+end
+  
+gem_group :features do
   gem 'database_cleaner' # For use in features with multi-threaded JS tests
   gem 'capybara'
-  gem 'poltergeist'             # Requires PhantomJS installed via brew/etc.
+  gem 'puffing-billy', require: 'billy' # Stubs for mock browsers
+  gem 'poltergeist' # Requires PhantomJS installed via brew/etc.
 end
 
 gem_group :test, :development do
-  gem 'rspec-rails'
+  # Dummy data
   gem 'factory_girl_rails'
   gem 'faker'
+  
+  # JS tests
   gem 'jasmine'
-  gem 'pry'
-  gem 'pry-nav'
+end
+
+group :console do
+  gem 'pry-rails', require: false
+  gem 'pry-nav', require: false
+  gem 'awesome_print', require: false
 end
 
 #
